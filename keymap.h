94bf3e8ac3e2f445b8e0648ccf089415d3ea0477
AsatothAbaddon <68546242+AsatothAbaddon@users.noreply.github.com>
2020-12-04 15:52:53 +0100

    Update and rename keymap.c to keymap.h

--- keymap.h
+++ keymap.h
@@ -19,7 +19,6 @@ enum layers {
   _0,
   _1,
   _2,
-  _ADJUST
 };
 
 /*
@@ -29,51 +28,51 @@ enum layers {
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
-/* Qwerty
+/* 0
  * ,-----------------------------------------------------------------------------------.
- * | Tab  |   Q  |   W  |   E  |   R  |   T  |   Y  |   U  |   I  |   O  |   P  | Bksp |
+ * | Esc/~|   Q  |   W  |   E  |   R  |   T  |   Y  |   U  |   I  |   O  |   P  | Bksp |
  * |------+------+------+------+------+------+------+------+------+------+------+------|
- * | Esc  |   A  |   S  |   D  |   F  |   G  |   H  |   J  |   K  |   L  |   ;  |  "   |
+ * | Tab  |   A  |   S  |   D  |   F  |   G  |   H  |   J  |   K  |   L  |   /  |  :   |
  * |------+------+------+------+------+------+------+------+------+------+------+------|
- * | Shift|   Z  |   X  |   C  |   V  |   B  |   N  |   M  |   ,  |   .  |   /  |Enter |
+ * | Shift|   Z  |   X  |   C  |   V  |   B  |   N  |   M  | - _  | < ,  |  . > |RS/Ent|
  * |------+------+------+------+------+------+------+------+------+------+------+------|
- * | BLTog|  ä   | Alt  | GUI  |Lower |    Space    |Raise | Left | Down |  Up  |Right |
+ * | Ctrl | Win  | Del  | Alt  | LT2/?|    Space    |LT1/! | Left | Down |  Up  |Right |
  * `-----------------------------------------------------------------------------------'
  */
 [_0] = LAYOUT_planck_mit(
-    KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_BSPC,
-    KC_ESC,  KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,
-    KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_ENT ,
-    RGB_TOG, UC(0x00E4), KC_LALT, KC_LGUI, LT(2,KC_QUES),   KC_SPC,  LT(1,KC_EXLM),   KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT
+    KC_GESC,  KC_Q,    KC_W,   KC_E,    KC_R,          KC_T,   KC_Y, KC_U,          KC_I,    KC_O,    KC_P,    KC_BSPC,
+    KC_TAB,   KC_A,    KC_S,   KC_D,    KC_F,          KC_G,   KC_H, KC_J,          KC_K,    KC_L,    KC_SCLN, KC_QUOT,
+    KC_LSFT,  KC_Z,    KC_X,   KC_C,    KC_V,          KC_B,   KC_N, KC_M,          KC_COMM, KC_DOT,  KC_SLSH, KC_SFTENT,
+    RGB_LCTL, KC_LGUI, KC_DEL, KC_LALT, LT(2,KC_QUES), KC_SPC,       LT(1,KC_EXLM), KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT
 ),
 
 /* 1
  * ,-----------------------------------------------------------------------------------.
- * |   ~  |   !  |   @  |   #  |   $  |   %  |   ^  |   &  |   *  |   (  |   )  | Bksp |
+ * | Esc  | 1    | 2    | 3    | 4    | 5    | 6    | 7    | 8    | 9    | 0    | Bksp |
+ * |------+------+------+------+------+------+------+------+------+------+------+------|                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                     
+ * |      |      |      |      |      |      |      |      |      |      |      |      |
  * |------+------+------+------+------+------+------+------+------+------+------+------|
- * | Del  |  F1  |  F2  |  F3  |  F4  |  F5  |  F6  |   _  |   +  |   {  |   }  |  |   |
+ * | Ä    | Ö    | Ü    |      |      |      |      |      |      |      |      |      |
  * |------+------+------+------+------+------+------+------+------+------+------+------|
- * |      |  F7  |  F8  |  F9  |  F10 |  F11 |  F12 |ISO ~ |ISO | | Home | End  |      |
- * |------+------+------+------+------+------+------+------+------+------+------+------|
- * |      |      |      |      |      |             |      | Next | Vol- | Vol+ | Play |
+ * | ä    | ö    | ü    |      |      |    Space    |      |      |      |      |      |
  * `-----------------------------------------------------------------------------------'
  */
 [_1] = LAYOUT_planck_mit(
-    KC_TILD, KC_EXLM, KC_AT,   KC_HASH, KC_DLR,  KC_PERC, KC_CIRC, KC_AMPR,    KC_ASTR,    KC_LPRN, KC_RPRN, KC_BSPC,
-    KC_DEL,  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_UNDS,    KC_PLUS,    KC_LCBR, KC_RCBR, KC_PIPE,
-    _______, KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,  S(KC_NUHS), S(KC_NUBS), KC_HOME, KC_END,  _______,
-    _______, _______, _______, _______, _______, _______, _______,    KC_MNXT,    KC_VOLD, KC_VOLU, KC_MPLY
+    KC_ESC, KC_P1, KC_P2, KC_P3,   KC_P4, KC_P5,  KC_P6, KC_P7, KC_P8,    KC_P9,    KC_P0, KC_BSPC,
+    _______,  _______,   _______,   _______,   _______,   _______,   _______,   _______,    _______,    _______, _______, _______,
+    UC(0x00C4), UC(0x00D6), UC(0x00DC),   KC_F9,   KC_F10,  KC_F11,  KC_F12,  S(KC_NUHS), S(KC_NUBS), KC_HOME, KC_END,  _______,
+    UC(0x00E4), UC(0x00F6), UC(0x00FC), _______, _______, _______, _______,    KC_MNXT,    KC_VOLD, KC_VOLU, KC_MPLY
 ),
 
 /* 2
  * ,-----------------------------------------------------------------------------------.
- * |   `  |   1  |   2  |   3  |   4  |   5  |   6  |   7  |   8  |   9  |   0  | Bksp |
+ * | F1   | F2   | F3   | F4   | F5   | F6   | F7   | F8   | F9   | F10  | F11  | F12  |
  * |------+------+------+------+------+------+------+------+------+------+------+------|
- * | Del  |  F1  |  F2  |  F3  |  F4  |  F5  |  F6  |   -  |   =  |   [  |   ]  |  \   |
+ * |      |      |      |      |      |      |      |      |      |      |      |      |
  * |------+------+------+------+------+------+------+------+------+------+------+------|
- * |      |  F7  |  F8  |  F9  |  F10 |  F11 |  F12 |ISO # |ISO / |Pg Up |Pg Dn |      |
+ * |      |      |      |      |      |      |      |      |      |      |      |      |
  * |------+------+------+------+------+------+------+------+------+------+------+------|
- * |      |      |      |      |      |             |      | Next | Vol- | Vol+ | Play |
+ * |      |      |      |      |      |      |      |      |      |      |      |      |
  * `-----------------------------------------------------------------------------------'
  */
 [_2] = LAYOUT_planck_mit(
@@ -115,3 +114,7 @@ layer_state_t layer_state_set_user(layer_state_t state) {
     _______, _______, _______, _______, _______, _______, _______, _______,  _______, _______, _______
 )
 */
+
+
+
+
